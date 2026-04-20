14f5f9dd2dd9ec765d9db51d42904a6adfa56887
Surajit <surajitjana20107@gmail.com>
2026-04-20 05:53:38 +0000

    remove unnecessary files

--- client/src/VideoPlayer.jsx
+++ client/src/VideoPlayer.jsx
@@ -7,8 +7,8 @@ const Player = createPlayer({ features: videoFeatures });
 export function VideoPlayer() {
   return (
     <Player.Provider>
-      <VideoSkin poster="https://c8b42fd5-fcba-484a-8a86-42ab247fcc30-00-2fc4x24q6z1os.sisko.replit.dev/uploads/77a2d636-8193-4593-ac28-37ca9f664c8b/index.m3u8">
-        <Video src="https://c8b42fd5-fcba-484a-8a86-42ab247fcc30-00-2fc4x24q6z1os.sisko.replit.dev/uploads/77a2d636-8193-4593-ac28-37ca9f664c8b/index.m3u8" playsInline />
+      <VideoSkin poster="https://c8b42fd5-fcba-484a-8a86-42ab247fcc30-00-2fc4x24q6z1os.sisko.replit.dev/uploads/42f5f97f-32f9-4aff-a18b-ff4a878c5cec/index.m3u8">
+        <Video src="https://c8b42fd5-fcba-484a-8a86-42ab247fcc30-00-2fc4x24q6z1os.sisko.replit.dev/uploads/42f5f97f-32f9-4aff-a18b-ff4a878c5cec/index.m3u8" playsInline />
       </VideoSkin>
     </Player.Provider>
   );
